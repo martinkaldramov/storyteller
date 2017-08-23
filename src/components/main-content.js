0bcbeb08b42b5f6e7b6029dfcc47e7f8540b80d6
Marto <martinkaldramov1@gmail.com>
2017-08-23 06:40:50 +0300

    Added 2 more components (sideMenu and a mainComponent)

---
+++ src/components/main-content.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class MainContent extends Component{
+  render(){
+    return(
+      <div>
+        <p><span>User</span> added a new story</p>
+	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/side-menu.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class SideMenu extends Component{
+  render(){
+    return(
+      <div>
+ 	<ul>
+          <li>Profile</li>
+	  <li>Stories Feed</li>
+	  <li>Browse Genres</li>
+	  <li>Saved Stories</li>
+	  <li>FAQ</li>
+        </ul>
+      </div>
+    )
+  }
+}
